97e20aafba55c99e1fd50cba8aec3bf17ba1dba4
granitnebiu <granitnebiu@gmail.com>
2022-06-13 15:05:16 +0200

    added React Hook Validation and functions to start login or sing up

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
+interface Inputs {
+  email: string
+  password: string
+}
 function Login() {
+  const [login, setLogin] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+  // const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (login) {
+      // await singIn(email, password)
+    } else {
+      // await singUp(email, password)
+    }
+  }
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -22,28 +44,50 @@ function Login() {
         height={150}
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:w-1/2 md:px-14 lg:w-1/4"
+      >
         <h1 className="text-4xl font-semibold">Sing In</h1>
         <div className="flex flex-col space-y-4">
           <label htmlFor="" className="w-full">
-            <input type="email" placeholder="Email" className="input" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
           <label htmlFor="" className="w-full">
             <input
               type="password"
-              name=""
               id=""
               placeholder="Password"
               className="input"
+              {...register('password', { required: true })}
             />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          onClick={() => setLogin(true)}
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+        >
           Sing In
         </button>
         <div className="text-[gray]">
           New to Netflix?
           <button
+            onClick={() => setLogin(false)}
             className="ml-1 cursor-pointer text-white hover:underline"
             type="submit"
           >
